e8d51b6e224f1c88108130918a0c006b116910c5
unknowntpo <e850506@gmail.com>
2020-08-21 06:55:57 +0800

    Add correct binary exponentiation implementation

--- fib/power.c
+++ fib/power.c
@@ -10,24 +10,27 @@
  * Ref: https://www.youtube.com/watch?v=L-Wzglnm4dM
  * at 2:59
  */
-long long power_wrong(long long a, long long b)
+long long power(long long a, long long b)
 {
     if (b == 0) return 1;
-    if (b == 1) return a;
-    long long tmp = power_wrong(a, b / 2);
-    long long result = tmp * tmp;
-    return result;
+    if (b == 0) return a;
+
+    long long res = power(a, b / 2);
+    res *= res;
+    if (b % 2)
+        res = a * res;
+    return res;
 } 
 int main(int argc, char **argv)
 {
-    if (argc != 3 || atoi(argv[1]) < 0 || atoi(argv[1]) < 0) {
-        printf("<Usage> : ./power [base] [power], base and power are greate than\n");
+    if (argc != 3 || atoi(argv[1]) < 0 || atoi(argv[2]) < 0) {
+        printf("<Usage> : ./power [base] [power], base and power are greater than 0\n");
         return 1;
     }
 
     long long a = atoi(argv[1]);
     long long b = atoi(argv[2]);
-    long long result = power_wrong(a, b);
+    long long result = power(a, b);
 
     printf("%lld\n", result);
     return 0;
