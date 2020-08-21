4d39839cbc2ee84738756ea58ef6cd8c8eb3dd2a
unknowntpo <e850506@gmail.com>
2020-08-21 08:04:31 +0800

    Add iterative version of binary exponentiation

--- fib/power.c
+++ fib/power.c
@@ -10,28 +10,40 @@
  * Ref: https://www.youtube.com/watch?v=L-Wzglnm4dM
  * at 2:59
  */
-long long power(long long a, long long b)
+long long power_recur(long long a, long long b)
 {
     if (b == 0) return 1;
     if (b == 0) return a;
 
-    long long res = power(a, b / 2);
+    long long res = power_recur(a, b / 2);
     res *= res;
     if (b % 2)
         res = a * res;
     return res;
 } 
+long long power_iter(long long a, long long b)
+{
+    long long result = 1;
+    while (b != 0) {
+        if (b % 2) result *= a;
+        a *= a;
+        b /= 2;
+    }
+    return result;
+}
 int main(int argc, char **argv)
 {
-    if (argc != 3 || atoi(argv[1]) < 0 || atoi(argv[2]) < 0) {
-        printf("<Usage> : ./power [base] [power], base and power are greater than 0\n");
+    if (argc != 3 || atoi(argv[1]) <= 0 || atoi(argv[2]) < 0) {
+        printf("<Usage> : ./power [base] [power], base > 0, power >= 0\n");
         return 1;
     }
 
     long long a = atoi(argv[1]);
     long long b = atoi(argv[2]);
-    long long result = power(a, b);
-
+#if 0
+    long long result = power_recur(a, b);
+#endif
+    long long result = power_iter(a, b);
     printf("%lld\n", result);
     return 0;
 }
