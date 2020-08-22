7be7fc0db7310045cabff37ba5d7e9de10d43d2e
unknowntpo <e850506@gmail.com>
2020-08-22 10:51:52 +0800

    Implement binary adder in c

--- adder/adder.c
+++ adder/adder.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int adder(int a, int b)
 {
-    int sum, carry;
-
-    sum = a ^ b;
-    carry = (a & b) << 1;
+    int sum = a ^ b;
+    int carry = a & b;
     if (carry == 0)
         return sum;
-    return adder(sum, carry);
+    adder(sum, carry << 1);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    int a=3, b=5;
+    if (argc != 3) {
+        printf("Usage: ./adder a b\n");
+        return -1;
+    }
+    int a=atoi(argv[1]);
+    int b=atoi(argv[2]);
     int result = adder(a, b);
     printf("%d + %d = %d", a, b, result);
 }
