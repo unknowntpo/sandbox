4bc9906212a06dadf9f9ed5628ae633830ac0960
unknowntpo <e850506@gmail.com>
2020-08-24 09:48:27 +0800

    Implement my version of merge sort array
    You can see the difference between my version and this website's
    version[1].
    I use for loop to explicitly showing the increment of counter,
    the website[1] use postfix increment and while loop to simplify this
    process, which is very elegent, but not very intuitive.
    
    [1]: https://www.thecrazyprogrammer.com/2014/03/c-program-for-implementation-of-merge-sort.html

--- leetcode/merge_sort_tutorial/merge_sort.c
+++ leetcode/merge_sort_tutorial/merge_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define DEBUG_MODE 1
-
+#define MAXSIZE 30
 
 
 void mergesort(int a[], int i, int j);
@@ -20,7 +20,7 @@ void p_output(int *a, int n)
 
 int main()
 {
-    int a[30], n, i;
+    int a[MAXSIZE], n, i;
     printf("Enter no of elements:");
     scanf("%d", &n);
     printf("Enter array elements:");
@@ -36,41 +36,52 @@ int main()
 
 void mergesort(int a[], int i, int j)
 {
-    int mid;
-#if DEBUG_MODE
-    p_output(a, j + 1);  // j + 1: size of array a
-#endif
-    if (i < j) {
-        mid = (i + j) / 2;
-        mergesort(a, i, mid);          // left recursion
-        mergesort(a, mid + 1, j);      // right recursion
-        merge(a, i, mid, mid + 1, j);  // merging of two sorted sub-arrays
-    }
+    // end condition
+    if (i == j)
+        return;
+    int mid = (i + j) / 2;
+    
+    mergesort(a, 0, mid);
+    mergesort(a, mid + 1, j);
+    merge(a, 0, mid, mid + 1, j);
+    return;
 }
 
 void merge(int a[], int i1, int j1, int i2, int j2)
 {
-    int temp[50];  // array used for merging
+    /* TODO: implement this with leetcode merge sorted array */
+    // actually, it's merge sorted array
     int i, j, k;
-    i = i1;  // beginning of the first list
-    j = i2;  // beginning of the second list
-    k = 0;
+    int temp[MAXSIZE];
+    
+    if (i1 == i2)
+        return;
 
-    while (i <= j1 && j <= j2)  // while elements in both lists
-    {
-        if (a[i] < a[j])
-            temp[k++] = a[i++];
-        else
-            temp[k++] = a[j++];
+    for (k = 0, i = i1, j = i2; (i <= j1) && (j <= j2) ; k++) {
+        if (a[i] < a[j]) {
+            temp[k] = a[i];
+            i++;
+        }
+        else {
+            temp[k] = a[j];
+            j++;    
+        }
+        if (i > j1) {
+            // copy rest of ele to temp, range from j to j2 
+            for (; j <= j2 ; j++, k++)
+                temp[k + 1] = a[j];
+            break;
+        }
+        if (j > j2) {
+            // copy rest of ele to temp, range from i to j1
+            for (; i <= j1 ; i++, k++)
+                temp[k + 1] = a[i];
+            break;
+        }
     }
-
-    while (i <= j1)  // copy remaining elements of the first list
-        temp[k++] = a[i++];
-
-    while (j <= j2)  // copy remaining elements of the second list
-        temp[k++] = a[j++];
-
-    // Transfer elements from temp[] back to a[]
-    for (i = i1, j = 0; i <= j2; i++, j++)
-        a[i] = temp[j];
+    
+    /* copy element in temp[] back to a[] */
+    for (i = 0; i <= k; i++)
+        a[i] = temp[i];
+    return;
 }
