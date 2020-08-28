80eb6d51eb28304a2b7aab5ea8a056050432ac7f
unknowntpo <e850506@gmail.com>
2020-08-28 15:29:10 +0800

    Delete unnecessary while loop check condition

--- leetcode/merge_sort_tutorial/merge_sort.c
+++ leetcode/merge_sort_tutorial/merge_sort.c
@@ -12,7 +12,7 @@ void p_output(int *a, int n)
     printf("Output: ");
     printf("[");
     for (int i = 0; i < n; i++)
-        printf("%d%s", a[i], (i < n - 1) ? " ": "");
+        printf("%d%s", a[i], (i < n - 1) ? " " : "");
     printf("]");
     printf("\n");
 }
@@ -37,47 +37,35 @@ void mergesort(int a[], int i, int j)
 {
     if (i == j)
         return;
-    
     int mid = (i + j) / 2;
-    
     mergesort(a, i, mid);
     mergesort(a, mid + 1, j);
     merge(a, i, mid, mid + 1, j);
-#if DEBUG_ON
-    printf("\ti = %d, j = %d\nafter merge", i, j);
-    p_output(a, j + 1);
-#endif
 }
 void merge(int a[], int i1, int j1, int i2, int j2)
 {
-    int k = 0, i = i1, j = i2;
-    int temp[MAXSIZE];
-#if DEBUG_ON
-    printf("\tmerge: i1 = %d, j1 = %d, i2 = %d, j2 = %d\n", i1, j1, i2, j2);
-#endif
     if (i1 == i2)
         return;
 
-    while (i <= j1 && j <=j2) {
-        if (a[i] < a[j])
+    int i = i1, j = i2, k = 0;
+    int temp[MAXSIZE];
+
+    while (i <= j1 && j <= j2) {
+        if (a[i] <= a[j])
             temp[k++] = a[i++];
         else
             temp[k++] = a[j++];
-
-        while (i > j1) {
-            while (j <= j2) 
-                temp[k++] = a[j++];
-            break;
-        }
-            
-        while (j > j2) {
-            while (i <= j1) 
-                temp[k++] = a[i++];
-            break;
-        }
     }
 
+    /* copy rest element */
+    while (j <= j2)  // i has exceeded j1
+        temp[k++] = a[j++];
+    while (i <= j1)  // j has exceeded j2
+        temp[k++] = a[i++];
+
+    /* copy element back to a[] */
     for (i = i1, k = 0; i <= j2; i++, k++)
         a[i] = temp[k];
+
     return;
 }
