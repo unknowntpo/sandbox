26e44ea3f932104e321097e229db341faf8fea39
e850506 <e850506@gmail.com>
2020-09-07 10:00:48 +0800

    Use destrcutor to reduce repeated code

--- php/fruit.php
+++ php/fruit.php
@@ -10,15 +10,19 @@ class Fruit
         $this->name = $name;
         $this->color = $color;
     }
+
+    function __destruct() {
+        echo "The fruit is {$this->name} and the color is {$this->color}.\n";
+    }
 }
 
 $apple = new Fruit("Apple", "Red");
 $banana = new Fruit("Banana", "Green");
 
-function show_fruit($fruit)
-{
-    echo "The fruit is '$fruit->name', and color is '$fruit->color'\n";
-}
+// function show_fruit($fruit)
+// {
+//     echo "The fruit is '$fruit->name', and color is '$fruit->color'\n";
+// }
 
-show_fruit($apple);
-show_fruit($banana);
+// show_fruit($apple);
+// show_fruit($banana);
