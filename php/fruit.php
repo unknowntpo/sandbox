2fd79ffbced18fe276c43fe0a2c7b0de3fc195f6
e850506 <e850506@gmail.com>
2020-09-07 09:54:18 +0800

    Add constructor for easily initialize our object

--- php/fruit.php
+++ php/fruit.php
@@ -4,49 +4,16 @@ class Fruit
     public $name;
     public $color;
 
-    /* Deal with name */
-    public function set_name($name)
+    /* Set name and color with __construct */
+    function __construct($name, $color)
     {
         $this->name = $name;
-    }
-    /* Deal with color */
-    public function set_color($color)
-    {
         $this->color = $color;
-    }
-
-    /* Play with pseudo variables $this */
-    public function check_this()
-    {
-        // print isset($this);              //true,   $this exists
-        // print gettype($this);            //Object, $this is an object
-        // print is_array($this);           //false,  $this isn't an array
-        // $array = get_object_vars($this);    //true,   $this's variables are an array
-        // foreach($array as $item) {
-        //     //print "$item\n";
-        //     printf("%s%s\n", $item, (!next($array)) ? "" : "");
-        // }
-        // print is_object($this);          //true,   $this is still an object
-        // print get_class($this);          //YourProject\YourFile\YourClass
-        // print get_parent_class($this);   //YourBundle\YourStuff\YourParentClass
-        // print gettype($this->name); //object
-        print_r($this);                  //delicious data dump of $this
-        // print $this->name;        //access $this variable with ->
-        echo "\n";
-        return;
     }
 }
 
-$apple = new Fruit();
-$banana = new Fruit();
-
-/* Set property of apple */
-$apple->set_name('Apple');
-$apple->set_color('red');
-
-/* Set property of banana */
-$banana->set_name('Banana');
-$banana->set_color('yellow');
+$apple = new Fruit("Apple", "Red");
+$banana = new Fruit("Banana", "Green");
 
 function show_fruit($fruit)
 {
@@ -55,5 +22,3 @@ function show_fruit($fruit)
 
 show_fruit($apple);
 show_fruit($banana);
-
-$banana->check_this();
