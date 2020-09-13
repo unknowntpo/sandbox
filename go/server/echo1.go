31001b5fc49f3c9971361f0d364a49fa55ffafcc
e850506 <e850506@gmail.com>
2020-09-13 15:09:54 +0800

    Implement a simple echo URL path web server
    Ref: GO Programming Language ch1.7 A Web Server

---
+++ go/server/echo1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "fmt"
+)
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1> Welcome to Message Filter~ </h1>\n")
+    fmt.Fprintf(w, "URL path = %s\n", r.URL.Path)
+}
+
+
