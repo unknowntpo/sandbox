9bacfae7ab8e8a1af59a62af071207aa422a551a
unknowntpo <e850506@gmail.com>
2020-09-16 10:58:46 +0800

    Implement switch mechanisam of different fib method
    This patch use concept of callback function to allow us to register
    different fibonacci counting method at runtime.
    
    Usage:
    <Usage> : ./fastfib [fibnum] [method number], fibnum >= 0
    
    method number:
        0: FIB_TAIL
        1: FIB_ITER
        2: FIB_FAST
        None of them: FIB_FAST
    
    TODO:
    - Explicitly report bug if user input wrong method number
    - How to display the Method table automatically?

--- fib/fastfib.c
+++ fib/fastfib.c
@@ -4,25 +4,32 @@
 #define DEBUG_MODE 1
 #define DEBUG(n) printf("fib[%lld]\n", n)
 
-long long fib_tail(long long n, long long a, long long b);
-long long fib(long long n);
+long long do_fib_tail(long long n, long long a, long long b);
+long long fib_tail(long long n);
 long long fib_iter(long long n);
-long long fastfib(long long n);
+long long fib_fast(long long n);
+void register_fib_method(long long n);
 /* Function pointer to selected fib function */
 long long (*fp)(long long n);
 
+enum {
+    FIB_TAIL,
+    FIB_ITER,
+    FIB_FAST,
+};
 
-long long fib_tail(long long n, long long a, long long b)
+
+long long do_fib_tail(long long n, long long a, long long b)
 {
     if (n == 0)
         return a;
 
-    return fib_tail(n - 1, b, a + b);
+    return do_fib_tail(n - 1, b, a + b);
 }
 /* Calling the actual tail recursion fib funciton */
-long long fib(long long n)
+long long fib_tail(long long n)
 {
-    return fib_tail(n, 0, 1);
+    return do_fib_tail(n, 0, 1);
 }
 
 
@@ -46,7 +53,7 @@ long long fib_iter(long long n)
 
     return result;
 }
-long long fastfib(long long n)
+long long fib_fast(long long n)
 {
 #if DEBUG_MODE
     DEBUG(n);
@@ -57,25 +64,51 @@ long long fastfib(long long n)
         return 1;
     if (n % 2) {
         long long k = (n - 1) / 2;
-        return fastfib(k + 1) * fastfib(k + 1) + fastfib(k) * fastfib(k);
+        return fib_fast(k + 1) * fib_fast(k + 1) + fib_fast(k) * fib_fast(k);
     } else {
         long long k = n / 2;
-        return fastfib(k) * (2 * fastfib(k + 1) - fastfib(k));
+        return fib_fast(k) * (2 * fib_fast(k + 1) - fib_fast(k));
     }
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2 || atoi(argv[1]) < 0) {
-        printf("<Usage> : ./fastfib [fibnum], fibnum >= 0\n");
+    /* TODO: Explicitly report bug if user input wrong method number */
+    /* TODO: How to display the Method table automatically? */
+    if (argc != 3 || atoi(argv[1]) < 0) {
+        printf("<Usage> : ./fastfib [fibnum] [method number], fibnum >= 0\n");
         return 1;
     }
-    /* TODO: Use array of function pointer to switch different fib method */
-    // fp = fastfib;
-    fp = fib_iter;
     long long n = atoll(argv[1]);
+    int method = atoi(argv[2]); 
+    /* Register fib method */
+    register_fib_method(method);
+
+    /* Do fib number counting */
     long long result = fp(n);
 
     printf("%lld\n", result);
     return 0;
+}
+
+void register_fib_method(long long n)
+{
+    /* Register fib method */
+    switch (n) {
+        case FIB_TAIL:
+            fp = fib_tail;
+            puts("tail recursion");
+            break;
+        case FIB_ITER:
+            fp = fib_iter;
+            puts("iteration");
+            break;
+        case FIB_FAST:
+            fp = fib_fast;
+            puts("fast doubling");
+            break;
+        default:
+            fp = fib_fast;
+            puts("default method");
+    }
 }
