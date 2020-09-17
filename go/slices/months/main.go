1bdbec580d08befcdc15c5fb9482c40a0906655c
unknowntpo <e850506@gmail.com>
2020-09-18 07:25:23 +0800

    Add Q1 slices for practice

--- go/slices/months/main.go
+++ go/slices/months/main.go
@@ -12,7 +12,10 @@ func main() {
 		7: "Jul", 8: "Aug", 9: "Sep",
 		10: "Oct", 11: "Nov", 12: "Dec"}
 	Q2 := months[4:7]
+        Q1 := months[1:4]
 	summer := months[6:9]
-        fmt.Println("Q2:\t", Q2)
+        fmt.Println("Q2:\t", Q2, "length:\t", len(Q2), "capacity:\t", cap(Q2))
+        fmt.Println("Q1:\t", Q1, "length:\t", len(Q1), "capacity:\t", cap(Q1))
+        fmt.Println("")
         fmt.Println("summer:\t", summer)
 }
