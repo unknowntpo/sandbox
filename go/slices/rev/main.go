121622694ef4c5f2bb81c13d8e6fd5667108266a
unknowntpo <e850506@gmail.com>
2020-09-21 08:32:33 +0800

    Practice rotating a slice left by n elements
    Ref:
    - My HackMD: https://hackmd.io/@unknowntpo/gopl#Ch42-Slices-B19-63
    - GOPL 4.2 Slices p.177

--- go/slices/rev/main.go
+++ go/slices/rev/main.go
@@ -3,17 +3,29 @@ package main
 // auto go fmt in vim ?
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func reverse(s []int) {
-    for i, j := 0, len(s) - 1; i < j; i, j = i + 1, j - 1{
-        s[i], s[j] = s[j], s[i]
-    }
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
 }
 
 func main() {
-    a := [...]int{0, 1, 2, 3, 4, 5}
-    reverse(a[:])
-    fmt.Println(a)
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Println("Original")
+	fmt.Println(a)
+
+	fmt.Println("First rotate")
+	reverse(a[:2])
+	fmt.Println(a)
+
+	fmt.Println("Second rotate")
+	reverse(a[2:])
+	fmt.Println(a)
+
+	fmt.Println("Last rotate")
+	reverse(a[:])
+	fmt.Println(a)
 }
