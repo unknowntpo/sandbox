df26e3468af82cea5feaca2d747921c356644c94
unknowntpo <e850506@gmail.com>
2020-09-21 08:15:17 +0800

    Implement in-place reverse of elements in slice
    Ref:
    GOPL 4.2 slices (p.176)
    My hackMD https://hackmd.io/@unknowntpo/gopl#Ch42-Slices-B19-63

---
+++ go/slices/rev/main.go
@@ -0,0 +1,19 @@
+package main
+
+// auto go fmt in vim ?
+
+import (
+    "fmt"
+)
+
+func reverse(s []int) {
+    for i, j := 0, len(s) - 1; i < j; i, j = i + 1, j - 1{
+        s[i], s[j] = s[j], s[i]
+    }
+}
+
+func main() {
+    a := [...]int{0, 1, 2, 3, 4, 5}
+    reverse(a[:])
+    fmt.Println(a)
+}
