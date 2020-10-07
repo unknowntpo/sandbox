5724500527a02a65b3e408caecebbcaf3dc12de9
unknowntpo <e850506@gmail.com>
2020-10-07 08:17:17 +0800

    Feat: show the binary representation of object
    This patch added new feature for showing the binary representation
    of object, which is great for debugging.

--- leetcode/29_Divide_Two_Integers/divid_int.c
+++ leetcode/29_Divide_Two_Integers/divid_int.c
@@ -1,18 +1,61 @@
+#include <endian.h>
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Turn it on to show the verbose output */
+#define DEBUG_ON 1
+
+/* Show_byte is the function in CS:APP CH2, showing the binary representation of
+ * object */
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t size)
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    for (size_t i = size; i > 0; i--)
+        printf("%.2x ", start[i]);
+#else
+    for (size_t i = 0; i < size; i++)
+        printf("%.2x ", start[i]);
+#endif
+    puts("");
+}
+/* Show the binary representation of long integer */
+void show_long(long a)
+{
+    show_bytes((byte_pointer) &a, sizeof(long));
+}
+
+void show_int(int a)
+{
+    show_bytes((byte_pointer) &a, sizeof(int));
+}
+
+
+/* Actual devide function doing the division operation */
 int divide(int dividend, int divisor)
 {
     /* Deal with special condition */
     if (dividend > INT_MAX || dividend < INT_MIN || divisor > INT_MAX ||
         divisor < INT_MIN || divisor == 0)
         return INT_MAX;
-
+#if DEBUG_ON
+    puts("dividend:");
+    show_int(dividend);
+    puts("divisor:");
+    show_int(divisor);
+#endif
     /* Cast to long integer for computing */
     long dvd = (long) abs(dividend);
     long dvs = (long) abs(divisor);
-
+#if DEBUG_ON
+    puts("dvd:");
+    show_long(dvd);
+    puts("dvs");
+    show_long(dvs);
+#endif
     /* Construct for answer */
     int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
     int ans = 0;
@@ -23,6 +66,9 @@ int divide(int dividend, int divisor)
             temp <<= 1;
             count <<= 1;
         }
+#if DEBUG_ON
+        printf("temp = %d\t count = %d\t ans = %d\n", temp, count, ans);
+#endif
         dvd -= temp;
         ans += count;
     }
