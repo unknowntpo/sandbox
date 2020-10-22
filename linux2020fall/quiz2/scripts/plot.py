9194ff0e92395e1f3347e74ed02b078ed854f11c
unknowntpo <e850506@gmail.com>
2020-10-22 20:22:16 +0800

    Modify input file location of perf.py

--- linux2020fall/quiz2/scripts/plot.py
+++ linux2020fall/quiz2/scripts/plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.loadtxt("../plot/out.txt")
+data = np.loadtxt("plot/out.txt")
 
 # Set limit of axis
 plt.xlim(2000, 3000)
@@ -9,5 +9,8 @@ plt.ylim(0, 100000)
 plt.plot(data[:, 0], data[:, 1], label="8 bytes/ time")
 plt.plot(data[:, 0], data[:, 2], label="1 byte/ time")
 plt.legend()
-plt.title("isascii calculate speed comparison: ")
+plt.title("isascii performance comparison: ")
+
+# save the output pic
+plt.savefig('isascii_perf.png')
 plt.show()
