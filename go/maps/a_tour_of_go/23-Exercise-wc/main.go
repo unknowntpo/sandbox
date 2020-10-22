97dff1e3ed2306b0328f83c2a52448710eb52c6f
unknowntpo <e850506@gmail.com>
2020-10-22 13:34:26 +0800

    Implement key collecting function of maps
    Ref:
        - [openHome - Maps](https://openhome.cc/Gossip/Go/Map.html)

--- go/maps/a_tour_of_go/23-Exercise-wc/main.go
+++ go/maps/a_tour_of_go/23-Exercise-wc/main.go
@@ -12,8 +12,9 @@ import (
 // value: amount of occurance of words
 // e.g. "I am learning Go!" ->  map[string]int{"Go!":1, "I":1, "am":1, "learning":1}
 func WordCount(s string) map[string]int {
-	// split the string s
+	// split the strings and put them to a slice
 	a := strings.Fields(s)
+	// I am a pig
 	m := make(map[string]int)
 	for _, v := range a {
 		m[v]++
