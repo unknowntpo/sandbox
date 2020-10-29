12283330c5faf404855f63e95a215e95a4125f20
unknowntpo <e850506@gmail.com>
2020-10-29 09:12:41 +0800

    Adjust function calling flow
    The new function calling flow is:
    compute()
        └── math.Pow()
        └── hypot()
        └── perim()
        ...

--- go/function/A-Tour-of-Go/func-values/main.go
+++ go/function/A-Tour-of-Go/func-values/main.go
@@ -10,8 +10,8 @@ import (
 // e.g.
 // 	compute34(hypot): compute the hypotenuse of triangle with side 3, 4
 //	compute34(math.Pow): compute 3 to the power of 4
-func compute34(fn func(float64, float64) float64) float64 {
-	return fn(3, 4)
+func compute(fn func(float64, float64) float64, x, y float64) float64 {
+	return fn(x, y)
 }
 
 func main() {
@@ -20,10 +20,15 @@ func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
 	}
-	fmt.Println("Hypotenuse of triangle with side 5, 12: ", hypot(5, 12))
+	// perim return the perimeter of given triangle
+	perim := func(x, y float64) float64 {
+		return x + y + hypot(x, y)
+	}
 
 	// compute the hypotenuse of triangle with side 3, 4
-	fmt.Println("Hypotenuse of triangle with side 3, 4: ", compute34(hypot))
+	fmt.Println("Hypotenuse of triangle with side 3, 4: ", compute(hypot, 3, 4))
 	// compute x to the power of y
-	fmt.Println("3 to the power of 4: ", compute34(math.Pow))
+	fmt.Println("3 to the power of 4: ", compute(math.Pow, 3, 4))
+	// compute the perimeter of given triangle
+	fmt.Println("Perimeter of triangle with side 5, 6: ", compute(perim, 5, 6))
 }
