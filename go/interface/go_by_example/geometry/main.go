7008fd0b00d31685636798ad2c6240a9724b179d
unknowntpo <e850506@gmail.com>
2020-11-03 10:48:15 +0800

    Practice using Method Pointer Receivers in Interfaces
    Ref:
        - [Go by example - Interfaces](https://gobyexample.com/interfaces)
        - [SENTRY: Method Pointer Receivers in Interfaces](https://sentry.io/answers/interface-pointer-receiver/)

--- go/interface/go_by_example/geometry/main.go
+++ go/interface/go_by_example/geometry/main.go
@@ -10,44 +10,41 @@ import (
 // define a measuring function taking in interface and output the result
 
 type geometry interface {
-	area() float64
 	perim() float64
+	area() float64
 }
 
 type rect struct {
-	length float64
-	width  float64
+	length, width float64
 }
-
 type circle struct {
 	radius float64
 }
 
-func (r rect) area() float64 {
-	return r.length * r.width
+func (c *circle) perim() float64 {
+	return math.Pi * c.radius * 2
 }
 
-func (r rect) perim() float64 {
-	return 2 * r.length + 2 * r.width
-}
-
-func (c circle) area() float64 {
+func (c *circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
+func (r *rect) perim() float64 {
+	return 2 * (r.length + r.width)
+}
+
+func (r *rect) area() float64 {
+	return r.length * r.width
 }
 
 func measure(g geometry) {
-	fmt.Printf("interface: %+v\n", g)
-	fmt.Println("area: ", g.area())
-	fmt.Println("perim: ", g.perim())
+	fmt.Println(g)
+	fmt.Println("Perimeter: ", g.perim())
+	fmt.Println("Area: ", g.area())
 }
-
 func main() {
-	r := rect{2, 4}
-	c := circle{3.5}
+	r := &rect{2, 4}
+	c := &circle{3.5}
 	measure(r)
 	measure(c)
 }
