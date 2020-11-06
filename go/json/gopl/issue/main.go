cbae692c055b7cd30c83778c159add026d24030c
unknowntpo <e850506@gmail.com>
2020-11-06 08:33:22 +0800

    Implement a github issue tracker with golang
    Usage
        $ ./issue <query string>
    Ref:
        - GOPL ch4 githubissuetracker
        - [Github: Construct a search query](https://docs.github.com/en/free-pro-team@latest/rest/reference/search#constructing-a-search-query)
        - [My HackMD](https://hackmd.io/@unknowntpo/gopl#45-JSON)

---
+++ go/json/gopl/issue/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	// how to import package in same src folder?
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: ./issue <query string>\n")
+		os.Exit(1)
+	}
+	result, err := SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n",
+			item.Number, item.User.Login, item.Title)
+	}
+}
---
+++ go/json/gopl/issue/search.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// SearchIssues queries the Github issue tracker.
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	// What does query do ?
+	q := url.QueryEscape(strings.Join(terms, " "))
+	fmt.Println("URL with query: ", IssuesURL+"?q="+q)
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	// For long-term stability, instead of http.Get, use the
+	// variant below which adds an HTTP request header indicating
+	// that only version 3 of the GitHub API is acceptable.
+	//
+	/*
+		req, err := http.NewRequest("GET", IssuesURL+"?q="+q, nil)
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set(
+			"Accept", "application/vnd.github.v3.text-match+json")
+		resp, err := http.DefaultClient.Do(req)
+	*/
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
