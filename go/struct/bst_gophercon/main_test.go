6728e2fae1ddce76cf9b2b05e717d17b5e9226bd
unknowntpo <e850506@gmail.com>
2020-11-07 10:13:45 +0800

    Adjust fib_counting folder position

---
+++ go/struct/bst_gophercon/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+/*
+Package, go doc support
+*/
+
+func TestMain(t *testing.T) {
+	// run test for all method
+
+}
+
+/* Sample testing workflow
+func main() {
+	// TODO: Write a test for every operation
+	// Define our nil root tree
+	t := &Tree{nil}
+	t.Insert(5)
+	t.Insert(3)
+	t.Insert(7)
+	t.Insert(2)
+	t.Insert(4)
+	t.Insert(6)
+	t.Insert(8)
+
+	// Show the tree
+	t.Show()
+
+	// Search the key that exist in tree
+	t.Search(3)
+	t.Search(4)
+	t.Search(10)
+}
+*/
