a696d40e13d6f7ddf20c11c2789cf494645b27e0
unknowntpo <e850506@gmail.com>
2020-11-12 09:16:48 +0800

    GWP Ch4 listing 4.3 - Read from request body

---
+++ go/gwp/ch4/4.3_reading_data_from_a_req_body/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func body(w http.ResponseWriter, r *http.Request) {
+	length := r.ContentLength
+	body := make([]byte, length)
+	r.Body.Read(body)
+	fmt.Fprintln(w, string(body))
+}
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/body", body)
+	server.ListenAndServe()
+}
