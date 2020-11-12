27d30ef3cd346b3e28e1b8029970739f17fadaa4
unknowntpo <e850506@gmail.com>
2020-11-12 09:25:09 +0800

    Implement my version of reading request body
    Using ioutil.ReadAll() to read the body of request.

--- go/gwp/ch4/4.3_reading_data_from_a_req_body/main.go
+++ go/gwp/ch4/4.3_reading_data_from_a_req_body/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func body(w http.ResponseWriter, r *http.Request) {
-	length := r.ContentLength
-	body := make([]byte, length)
-	r.Body.Read(body)
+	// GWP's version of reading body
+	/*
+		length := r.ContentLength
+		body := make([]byte, length)
+		r.Body.Read(body)
+		fmt.Fprintln(w, string(body))
+	*/
+	// my version of read reading the body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
 	fmt.Fprintln(w, string(body))
 }
 func main() {
