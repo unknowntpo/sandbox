bfaa99c4b43a6b267ba14128b19962648c896ba6
unknowntpo <e850506@gmail.com>
2020-11-23 07:44:18 +0800

    Implement Area(), Perimeter() method for rectangle and circle

--- go/Learn-go-with-tests/struct-method-interface/main.go
+++ go/Learn-go-with-tests/struct-method-interface/main.go
@@ -1,14 +1,28 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(r Rectangle) float64 {
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- go/Learn-go-with-tests/struct-method-interface/main_test.go
+++ go/Learn-go-with-tests/struct-method-interface/main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	r := Rectangle{10.0, 10.0}
+	got := r.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -13,11 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	t.Run("rectanges", func(t *testing.T) {
+		r := Rectangle{12.0, 6.0}
+		got := r.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		c := Circle{10}
+		got := c.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
