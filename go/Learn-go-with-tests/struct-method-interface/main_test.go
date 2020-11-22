169a22b5b12765cb4fe5d4e842759e239d6668c1
unknowntpo <e850506@gmail.com>
2020-11-23 07:54:53 +0800

    Use helper function to prevent duplicated area checking
    TODO: Use interface to encapsulate rectangle and circle as shape.

--- go/Learn-go-with-tests/struct-method-interface/main_test.go
+++ go/Learn-go-with-tests/struct-method-interface/main_test.go
@@ -13,24 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectanges", func(t *testing.T) {
-		r := Rectangle{12.0, 6.0}
-		got := r.Area()
-		want := 72.0
-
+	checkArea := func(t *testing.T, got, want float64) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 
+	}
+	t.Run("rectanges", func(t *testing.T) {
+		r := Rectangle{12.0, 6.0}
+		got := r.Area()
+		want := 72.0
+		checkArea(t, got, want)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		c := Circle{10}
 		got := c.Area()
 		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, got, want)
 	})
 }
