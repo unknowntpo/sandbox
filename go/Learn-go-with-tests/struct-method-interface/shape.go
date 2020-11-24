26ff5ad50f594ce98fdb4607cda9c1a69e4f7214
unknowntpo <e850506@gmail.com>
2020-11-24 14:52:42 +0800

    Add new shape: triangle
    This patch add a new shape: triangle,
    and write corresponding test for it.

--- go/Learn-go-with-tests/struct-method-interface/shape.go
+++ go/Learn-go-with-tests/struct-method-interface/shape.go
@@ -11,6 +11,10 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
 type Shape interface {
 	Area() float64
 }
@@ -29,4 +33,8 @@ func (c Circle) Perimeter() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
 }
