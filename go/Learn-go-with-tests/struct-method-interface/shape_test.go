aa7df484e664b6375e320808296ac2eece1ac67e
unknowntpo <e850506@gmail.com>
2020-11-24 15:09:35 +0800

    Use t.Run to split all test case
    This patch use t.Run at every test table element, which can make the
    test easier to debug, easy to know which test case is failed.

--- go/Learn-go-with-tests/struct-method-interface/shape_test.go
+++ go/Learn-go-with-tests/struct-method-interface/shape_test.go
@@ -14,18 +14,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		if got := tt.shape.Area(); got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.shape.Area(); got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+
+		})
 	}
 
 }
