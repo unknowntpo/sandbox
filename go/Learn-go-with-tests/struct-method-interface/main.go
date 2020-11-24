28e4fa40ced1cd4460694af8eeb4f57f6bc5c190
unknowntpo <e850506@gmail.com>
2020-11-24 14:29:23 +0800

    Implement interface Shape to wrap all shapes up

--- go/Learn-go-with-tests/struct-method-interface/main.go
+++ go/Learn-go-with-tests/struct-method-interface/main.go
@@ -11,6 +11,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Shape interface {
+	Perimeter() float64
+	Area() float64
+}
+
 func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
--- go/Learn-go-with-tests/struct-method-interface/main_test.go
+++ go/Learn-go-with-tests/struct-method-interface/main_test.go
@@ -13,8 +13,9 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, got, want float64) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
 		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
@@ -22,15 +23,13 @@ func TestArea(t *testing.T) {
 	}
 	t.Run("rectanges", func(t *testing.T) {
 		r := Rectangle{12.0, 6.0}
-		got := r.Area()
 		want := 72.0
-		checkArea(t, got, want)
+		checkArea(t, r, want)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		c := Circle{10}
-		got := c.Area()
 		want := 314.1592653589793
-		checkArea(t, got, want)
+		checkArea(t, c, want)
 	})
 }
