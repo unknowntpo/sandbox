7ef0229721e5b427fc9c84a2de53253b0c8a59cc
unknowntpo <e850506@gmail.com>
2020-11-30 14:09:59 +0800

    Handle the case: struct with nested structure
    This patch check if the input struct has embedded field,
    if that's the case, recursively call walk to examine the embedded field.

--- go/Learn-go-with-tests/reflection/reflection_test.go
+++ go/Learn-go-with-tests/reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	// Test table
 	cases := []struct {
@@ -34,6 +44,14 @@ func TestWalk(t *testing.T) {
 				Age  int
 			}{"Chris", 33},
 			[]string{"Chris"},
+		},
+		{
+			"Struct with nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
 		},
 	}
 	for _, test := range cases {
