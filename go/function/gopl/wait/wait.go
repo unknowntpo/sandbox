b6f925b4c738138936ddd8c01875140bae94c67e
unknowntpo <e850506@gmail.com>
2020-12-02 07:55:04 +0800

    Implement charcount and test
    This commit is from torbiak's gopl solution of ex 11.1.
    
    Usage:
            $ ./charcount << EOF
            heredoc> Hello 世界
            heredoc> EOF
    
    or:
            $ echo 'hello 世界123' | ./charcount
    
    Output:
            map[ASCII_Hex_Digit:1 Hex_Digit:1 Ideographic:2 Pattern_White_Space:2 Unified_Ideograph:2 White_Space:2]
            rune	count
            'l'	2
            'o'	1
            ' '	1
            '世'	1
            '界'	1
            '\n'	1
            'H'	1
            'e'	1
    
            len	count
            1	7
            3	2
    
            category                       count
            White_Space                    2
            Unified_Ideograph              2
            Ideographic                    2
            Hex_Digit                      1
            ASCII_Hex_Digit                1
            Pattern_White_Space            2
    Test:
        $ go test -v
    
    Ref:
        - [torbiak's repo](https://github.com/torbiak/gopl)
        - [Tests](https://github.com/torbiak/gopl/blob/master/ex11.1/charcount_test.go)
        - [solution](https://github.com/torbiak/gopl/blob/master/ex11.1/charcount.go)

--- go/function/gopl/wait/wait.go
+++ go/function/gopl/wait/wait.go
@@ -9,7 +9,7 @@ import (
 )
 
 func WaitForServer(url string) error {
-	const timeout = 1 * time.Minute
+	const timeout = 1 * time.Second
 	// explain
 	deadline := time.Now().Add(timeout)
 	for tries := 0; time.Now().Before(deadline); tries++ {
