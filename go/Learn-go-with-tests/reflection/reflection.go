7328dc4fa3c1fa71d647efcd5196f8f794e3025e
unknowntpo <e850506@gmail.com>
2020-12-02 10:46:43 +0800

    Refactor: generalize get field progress
    This commit refactor the code for get field of val.
    
    Ref:
        - [Learn go with test - reflection](https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reflection#refactor-5)

--- go/Learn-go-with-tests/reflection/reflection.go
+++ go/Learn-go-with-tests/reflection/reflection.go
@@ -6,21 +6,40 @@ func walk(x interface{}, fn func(input string)) {
 	// val := reflect.ValueOf(x)
 	val := getValue(x)
 
+	numberOfValues := 0
+	var getField func(int) reflect.Value
 	switch val.Kind() {
 	case reflect.Struct:
 		// walk through all the fields in struct
-		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
-		}
+		numberOfValues = val.NumField()
+		//?
+		getField = val.Field
+		/*
+			for i := 0; i < val.NumField(); i++ {
+				walk(val.Field(i).Interface(), fn)
+			}
+		*/
 	case reflect.Slice:
 		// walk through all the elements in slice
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
+		numberOfValues = val.Len()
+		//?
+		getField = val.Index
+		/*
+			for i := 0; i < val.Len(); i++ {
+				walk(val.Index(i).Interface(), fn)
+			}
+		*/
 	case reflect.String:
 		// reach what we want, call fn()
+		numberOfValues = 0
 		fn(val.String())
 	}
+
+	// if val is a slice, it will not get into this loop
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+
 }
 
 func getValue(x interface{}) reflect.Value {
