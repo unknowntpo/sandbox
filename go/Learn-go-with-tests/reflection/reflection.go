05fef1c02ac6c7592822f66146f0af85bffc3072
unknowntpo <e850506@gmail.com>
2020-12-04 10:45:01 +0800

    Refactor: generalize walk through value progress
    This commit introduce walkValue() to do the job of calling walk().
    
    TODO: Fix the non-ordered map bug
    Because the map key is non-ordered, so if you run test for several
    times:
    
    $ go test -v --run "TestWalk/Maps"
    
    You will get:
    
    === RUN   TestWalk
    === RUN   TestWalk/Maps
        reflection_test.go:98: got [Boz Bar], want [Bar Boz]
    --- FAIL: TestWalk (0.00s)
        --- FAIL: TestWalk/Maps (0.00s)
    FAIL
    exit status 1
    FAIL	_/Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/reflection	0.009s

--- go/Learn-go-with-tests/reflection/reflection.go
+++ go/Learn-go-with-tests/reflection/reflection.go
@@ -6,33 +6,29 @@ func walk(x interface{}, fn func(input string)) {
 	// val := reflect.ValueOf(x)
 	val := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 	switch val.Kind() {
 	case reflect.Struct:
 		// walk through all the fields in struct
-		numberOfValues = val.NumField()
-		getField = val.Field
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
 		// walk through all the elements in slice
-		numberOfValues = val.Len()
-		getField = val.Index
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 	case reflect.Map:
 		// walk through all elems in map
 		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
 	case reflect.String:
 		// reach what we want, call fn()
-		numberOfValues = 0
 		fn(val.String())
 	}
-
-	// if val is a slice, it will not get into this loop
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
-	}
-
 }
 
 func getValue(x interface{}) reflect.Value {
