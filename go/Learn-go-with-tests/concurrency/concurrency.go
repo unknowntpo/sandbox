930f7502b0ece2f82cd507c857ec79d8cd1a7491
unknowntpo <e850506@gmail.com>
2020-12-15 08:25:34 +0800

    Bugfix attempt: copy the value in shared variable
    This commit try to copy the value of shared variable 'url',
    not to directly access it.
    
    But the problem still there, becaue we try to write to the map 'results' at
    differrent goroutines at the same time, we got race condition.
    
    You can use
    
    $ go test -race
    
    to detect race condition.
    
    TODO: Fix Race condition of writting to map 'results' at the same time.
    
    Here's the bug:
    
    - Bug: concurrent map writes
    
    When we run
    
    $ go test -v --bench=.
    
    We get:
    
    fatal error: concurrent map writes
    
    goroutine 20 [running]:
    runtime.throw(0x114a8c1, 0x15)
            /usr/local/opt/go/libexec/src/runtime/panic.go:1116 +0x72 fp=0xc00002b718 sp=0xc00002b6e8 pc=0x1035172
    runtime.mapassign_faststr(0x1124920, 0xc000092300, 0x114ba2f, 0x1a, 0x0)
            /usr/local/opt/go/libexec/src/runtime/map_faststr.go:211 +0x3f1 fp=0xc00002b780 sp=0xc00002b718 pc=0x1013511
    _/Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency.CheckWebsites.func1(0x1150cb8, 0xc000092300, 0x114ba2f, 0x1a)
            /Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency/concurrency.go:15 +0x71 fp=0xc00002b7c0 sp=0xc00002b780 pc=0x110def1
    runtime.goexit()
            /usr/local/opt/go/libexec/src/runtime/asm_amd64.s:1374 +0x1 fp=0xc00002b7c8 sp=0xc00002b7c0 pc=0x1069ae1
    created by _/Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency.CheckWebsites
            /Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency/concurrency.go:14 +0x95
    
    goroutine 1 [chan receive]:
    testing.(*T).Run(0xc000082600, 0x11497f9, 0x11, 0x1150cb0, 0x107dee6)
            /usr/local/opt/go/libexec/src/testing/testing.go:1179 +0x3ad
    testing.runTests.func1(0xc000082480)
            /usr/local/opt/go/libexec/src/testing/testing.go:1449 +0x78
    testing.tRunner(0xc000082480, 0xc00009ade0)
            /usr/local/opt/go/libexec/src/testing/testing.go:1127 +0xef
    testing.runTests(0xc0000b4040, 0x1223650, 0x1, 0x1, 0xbfee1eb1c95c9370, 0x8bb2ce397b, 0x122b280, 0x100d150)
            /usr/local/opt/go/libexec/src/testing/testing.go:1447 +0x2e8
    testing.(*M).Run(0xc0000c8000, 0x0)
            /usr/local/opt/go/libexec/src/testing/testing.go:1357 +0x245
    main.main()
            _testmain.go:45 +0x138
    
    goroutine 18 [sleep]:
    time.Sleep(0x77359400)
            /usr/local/opt/go/libexec/src/runtime/time.go:188 +0xbf
    _/Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency.CheckWebsites(0x1150cb8, 0xc00002a740, 0x3, 0x3, 0xc0000ce08a)
            /Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency/concurrency.go:18 +0xb4
    _/Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency.TestCheckWebsites(0xc000082600)
            /Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency/concurrency_test.go:27 +0x167
    testing.tRunner(0xc000082600, 0x1150cb0)
            /usr/local/opt/go/libexec/src/testing/testing.go:1127 +0xef
    created by testing.(*T).Run
            /usr/local/opt/go/libexec/src/testing/testing.go:1178 +0x386
    exit status 2
    FAIL    _/Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency       0.010s

--- go/Learn-go-with-tests/concurrency/concurrency.go
+++ go/Learn-go-with-tests/concurrency/concurrency.go
@@ -1,14 +1,20 @@
 package concurrency
 
+import (
+	"time"
+)
+
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		go func() {
-			results[url] = wc(url)
-		}()
+		// Copy the value of url to avoid access variable 'url' at the same time
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
 	}
+	time.Sleep(2 * time.Second)
 	return results
 }
