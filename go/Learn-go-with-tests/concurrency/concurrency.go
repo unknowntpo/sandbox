d5ab02a25f8500d88ae26f0e67226a7b1d354429
unknowntpo <e850506@gmail.com>
2020-12-16 08:17:04 +0800

    Bugfix: Race condition
    This commit use channel to send result computed by sub-goroutine and
    send them to main goroutine, and the main goroutine write data to map
    to avoid race condition.
    
    And the preformance of this version is 40-times better
    
    === RUN   TestCheckWebsites
    --- PASS: TestCheckWebsites (0.00s)
    goos: darwin
    goarch: amd64
    BenchmarkCheckWebsites
    BenchmarkCheckWebsites-4   	      40	  28203491 ns/op
    PASS
    ok  	_/Users/tacomaker/repo/sandbox/go/Learn-go-with-tests/concurrency	1.190s
    
    Ref:
        - [LGWT-Concurrency](https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/concurrency#channels)

--- go/Learn-go-with-tests/concurrency/concurrency.go
+++ go/Learn-go-with-tests/concurrency/concurrency.go
@@ -1,20 +1,27 @@
 package concurrency
 
-import (
-	"time"
-)
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		// Copy the value of url to avoid access variable 'url' at the same time
 		go func(u string) {
-			results[u] = wc(u)
+			//results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
-	time.Sleep(2 * time.Second)
+	//time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 	return results
 }
