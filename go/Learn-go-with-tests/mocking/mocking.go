039a0eb48a8cec9f09bd7c416d145fb78f63df2b
unknowntpo <e850506@gmail.com>
2020-12-16 13:44:08 +0800

    Mocking: Sleeper interface
    This commit use Sleeper interface to let test code choose not to wait
    1 second, instead, it just record the amount of calling sleeper.Sleep()
    method.
    And in main(), we assign our sleeper to be DefaultSleeper, which
    actually sleep for 1 second.
    
    Ref:
        - [LGWT - Mocking](https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking#write-enough-code-to-make-it-pass-2)

--- go/Learn-go-with-tests/mocking/mocking.go
+++ go/Learn-go-with-tests/mocking/mocking.go
@@ -4,18 +4,33 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- go/Learn-go-with-tests/mocking/mocking_test.go
+++ go/Learn-go-with-tests/mocking/mocking_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	want := `3
 2
@@ -12,10 +20,14 @@ func TestCountdown(t *testing.T) {
 Go!`
 
 	buf := &bytes.Buffer{}
-	Countdown(buf)
+	spySleeper := &SpySleeper{}
+	Countdown(buf, spySleeper)
 	got := buf.String()
 
 	if got != want {
 		t.Fatalf("got %s, want %s", got, want)
+	}
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
 	}
 }
