b6851d7f53d9b64c71f784ecec4151e622d6d849
unknowntpo <e850506@gmail.com>
2020-12-18 10:46:10 +0800

    Implement checking sleep write sequence
    This commit check whether the sequence of Sleep() and
    Write() something to io.Writer,
    e.g.
    
    Sleep
    Sleep
    Sleep
    Sleep
    3
    2
    1
    Go!
    
    will pass the previous test.
    
    Ref:
        - [LGWT - mocking](https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking#still-some-problems)

--- go/Learn-go-with-tests/mocking/mocking_test.go
+++ go/Learn-go-with-tests/mocking/mocking_test.go
@@ -2,32 +2,58 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
+const write = "write"
+const sleep = "sleep"
+
+type CountdownOperationsSpy struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 func TestCountdown(t *testing.T) {
-	want := `3
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &CountdownOperationsSpy{})
+
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	buf := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
-	Countdown(buf, spySleeper)
-	got := buf.String()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
 
-	if got != want {
-		t.Fatalf("got %s, want %s", got, want)
-	}
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
-	}
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
