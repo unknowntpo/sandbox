bc3e5b0de4be646d808b80d2c8d360e1ce4ad4d4
unknowntpo <e850506@gmail.com>
2020-12-21 09:32:22 +0800

    Implement select to wait for channels to finish with timeout
    This commit use select to wait for multiple channel to finish it's
    job, and after a timeout, our Race() function will return error.
    
    Ref:
        - [LGWT - Select]((https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/select#timeouts)

--- go/Learn-go-with-tests/select/select.go
+++ go/Learn-go-with-tests/select/select.go
@@ -1,25 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		return a
-	} else {
-		return b
+func Racer(a, b string) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timeout waiting for %s and %s", a, b)
 	}
-
-	return
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	_, _ = http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
