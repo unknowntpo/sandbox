c67820c55ed07c18d751dd4539519814847052fe
unknowntpo <e850506@gmail.com>
2020-12-23 09:40:24 +0800

    Change test resource from strings.Reader() to os.File
    This commmit change test resource of database from strings.Reader() to os.File
    , because strings.Reader() doesn't have Write() method,
    which is necessary for POST request.
    And we creat a helper function createTempFile() to wrap up temporary file creating,
    and clean-up process.
    
    Ref:
        - [LGWT - IO / sorting](https://quii.gitbook.io/learn-go-with-tests/build-an-application/io#write-the-test-first-2)

--- go/Learn-go-with-tests/http_server/file_system_store_test.go
+++ go/Learn-go-with-tests/http_server/file_system_store_test.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("/league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+		initData := `[
             {"Name": "Cleo", "Wins": 10},
-            {"Name": "Chris", "Wins": 33}]`)
+            {"Name": "Chris", "Wins": 33}]`
+		database, cleanDatabase := createTempFile(t, initData)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -22,8 +26,31 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertLeague(t, got, want)
 
-		// call store.GetLeague() again to check seek problem
+		// call store.getleague() again to check seek problem
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+}
+
+// createTempFile create a temporary file with initial data specified by caller,
+// and return that file with the clean up function removeFile, caller can use it to
+// clean up temporary file easilly with defer keyword.
+func createTempFile(t *testing.T, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpfile, err := ioutil.TempFile("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
