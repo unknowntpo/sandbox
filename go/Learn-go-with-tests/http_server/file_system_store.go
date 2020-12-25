bdaeb7c53b5b9a7491cc5a22bf4cfc71ebf85140
unknowntpo <e850506@gmail.com>
2020-12-25 09:57:03 +0800

    Implement GetPlayerScore and RecordWin at file_system_store
    This commit implement GetPlayerScore and RecordWin at file_system_store
    , with tests below:
    
        - store wins for existing players
        - store wins for non-existing players
        - get player not found error
    
    Ref:
        - [LGWT - IO / sorting](https://quii.gitbook.io/learn-go-with-tests/build-an-application/io#write-enough-code-to-make-it-pass-2)

--- go/Learn-go-with-tests/http_server/file_system_store.go
+++ go/Learn-go-with-tests/http_server/file_system_store.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 )
 
 type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) (int, error) {
+	// TODO: Handle dummy return values
+	league := f.GetLeague()
+	for _, player := range league {
+		if player.Name == name {
+			return player.Wins, nil
+		}
+	}
+	// player not found
+	return 0, ErrPlayerNotFound
+
+}
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	// TODO: record wins toleauge table
+	league := f.GetLeague()
+
+	hasPlayer := false
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+			//player.Wins++
+			hasPlayer = true
+		}
+	}
+
+	// player not found
+	// same underlying array?
+	if hasPlayer == false {
+		league = append(league, Player{Name: name, Wins: 1})
+	}
+
+	// Write league back to our database
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(league)
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
--- go/Learn-go-with-tests/http_server/file_system_store_test.go
+++ go/Learn-go-with-tests/http_server/file_system_store_test.go
@@ -8,10 +8,12 @@ import (
 )
 
 func TestFileSystemStore(t *testing.T) {
-	t.Run("/league from a reader", func(t *testing.T) {
-		initData := `[
+	// data for our test database
+	initData := `[
             {"Name": "Cleo", "Wins": 10},
             {"Name": "Chris", "Wins": 33}]`
+
+	t.Run("/league from a reader", func(t *testing.T) {
 		database, cleanDatabase := createTempFile(t, initData)
 		defer cleanDatabase()
 
@@ -30,6 +32,55 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, initData)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Chris")
+
+		got, _ := store.GetPlayerScore("Chris")
+
+		want := 34
+		assertScoreEquals(t, got, want)
+	})
+	t.Run("store wins for non-existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, initData)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Pepper")
+
+		got, _ := store.GetPlayerScore("Pepper")
+
+		want := 1
+		assertScoreEquals(t, got, want)
+	})
+	t.Run("get player not found error", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, initData)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		_, err := store.GetPlayerScore("Pepper")
+
+		if err == nil {
+			t.Errorf("expect an error, but got nothing")
+		}
+
+		if err != ErrPlayerNotFound {
+			t.Errorf("wrong error message: got %v, want %v", ErrPlayerNotFound, err)
+		}
+	})
+}
+
+func assertScoreEquals(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
 
 // createTempFile create a temporary file with initial data specified by caller,
