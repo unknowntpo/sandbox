921ca79523ef9b76a405bc9da40ba68b64e1ae57
unknowntpo <e850506@gmail.com>
2020-12-25 08:08:28 +0800

    Use rsc.io/quote/v3 to output Proverb
    This commit output Proverb using rsc.io/quote/v3 module.
    
    Ref:
        - [The Go Blog - Using Go modules - Adding a dependency on a new major version](https://blog.golang.org/using-go-modules)

--- go/gomod/hello.go
+++ go/gomod/hello.go
@@ -1,7 +1,14 @@
 package hello
 
-import "rsc.io/quote"
+import (
+	"rsc.io/quote"
+	quotev3 "rsc.io/quote/v3"
+)
 
 func Hello() string {
 	return quote.Hello()
+}
+
+func Proverb() string {
+	return quotev3.Concurrency()
 }
