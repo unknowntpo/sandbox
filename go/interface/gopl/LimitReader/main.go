6191915ce1c1b8005ded5bbbe9339046bd545dce
Chang Chen Chien <e850506@gmail.com>
2021-01-27 08:44:40 +0800

    Implement io.LimitReader with some problem
    FIXME: In source code, why do we need to check the length of buffer p ?
    TODO: How to write test to validate correctness ?
    
    Reference:
        - [src/io/io.go](https://golang.org/src/io/io.go?s=15184:15241#L438)
        - [src/io/io_test.go](https://golang.org/src/io/io_test.go)

--- go/interface/gopl/LimitReader/main.go
+++ go/interface/gopl/LimitReader/main.go
@@ -9,18 +9,45 @@ import (
 	"strings"
 )
 
+// A LimitedReader reads from R but limits the amount of data returned to just
+// N bytes. Each call to Read updates N to reflect the new amount remaining.
+// Read returns EOF when N <= 0 or when the underlying R returns EOF.
 type limitedReader struct {
 	R io.Reader // underlying reader
 	N int64     // n bytes remained
 }
 
-// Our implementation of io.LimitReader,
-// It read n bytes from an io.Reader r, and return another io.Reader
-// that reads from r but reports an end-of-file condition after n
-// bytes.
-func limitReader(r io.Reader, n int64) io.Reader {
-	return r
+// Read reads from l.R, but limits the amount of data returned to just
+// N bytes. Each calll to Read updates N to reflect the new amount remaining.
+// Read returns EOF when N <= 0 or when the underlying R returns EOF.
+func (l *limitedReader) Read(p []byte) (n int, err error) {
+	if l.N <= 0 {
+		err = io.EOF
+		return
+	}
+
+	// FIXME: In source code, why do we need this check ?
+	// Ref: https://golang.org/src/io/io.go?s=15184:15241#L438
+	// if int64(len(p)) > l.N
+
+	// read n bytes from underlying io.Reader l.R
+	n, err = l.R.Read(p)
+	if err != nil {
+		return 0, err
+	}
+	// l.N += n
+	l.N -= int64(n)
+	return n, nil
 }
+
+// LimitReader returns a io.Reader that reads from r but stops with EOF after n
+// bytes. The underlying implementation is a *LimitedReader.
+func limitReader(r io.Reader, n int64) io.Reader {
+	// copy elements in r to *limitedReader l
+	l := &limitedReader{r, n}
+	return l
+}
+
 func main() {
 	r := strings.NewReader("some io.Reader stream to be read\n")
 	lr := io.LimitReader(r, 4)
