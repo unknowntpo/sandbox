cafa78481f7a37687a96361eef22565296c85d0e
Chang Chen Chien <e850506@gmail.com>
2021-04-14 10:45:24 +0800

    Move hashmap implementation to github.com/unknowntpo/hashmap

--- leetcode/1_twoSum/twoSum_ht.go
+++ leetcode/1_twoSum/twoSum_ht.go
@@ -2,80 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/unknowntpo/hashmap"
 )
 
-type Map struct {
-	bit int
-	ht  []htHead
-}
-
-type htHead struct {
-	first *htNode // points to first element of that bucket
-}
-
-type htNode struct {
-	key, value int
-	next       *htNode
-}
-
-func (m *Map) Init(bit int) {
-	m.bit = bit
-	size := 1 << bit
-	m.ht = make([]htHead, size)
-}
-
-func (h *htHead) appendHtNode(key, value int) {
-	indirect := &h.first
-	for ; (*indirect) != nil; indirect = &(*indirect).next {
-		if (*indirect).key == key {
-			// key exist, no need to append
-			return
-		}
-	}
-
-	// at tail of list, append newNode
-	newNode := &htNode{key: key, value: value, next: nil}
-	(*indirect) = newNode
-}
-
-func (m *Map) Add(key, value int) {
-	set := key % (2 << m.bit)
-	// access m.ht[set]
-	// if m.ht[set] not exist, append m.ht[]
-	/*
-		for len(m.ht) < set {
-			m.ht = append(m.ht, htHead{first: nil})
-		}
-	*/
-	// then append htNode
-	m.ht[set].appendHtNode(key, value)
-}
-
-func (m *Map) Get(key int) (value int, ok bool) {
-	set := key % (2 << m.bit)
-	// access m.ht[set]
-	// if m.ht[set] not exist, means key not exist in map
-	if len(m.ht) < set {
-		return 0, false
-	}
-
-	for node := m.ht[set].first; node != nil; node = node.next {
-		if node.key == key {
-			// key exist, return the value and ok
-			return node.value, true
-		}
-	}
-	// key not found
-	return 0, false
-}
-
-// String shows the whole map with format Map[key:value]
-func (m *Map) String() {
-	// figure out some method to contain all keys
-}
-
 func main() {
-	m := &Map{}
+	m := &hashmap.Map{}
 	bit := 10
 	m.Init(bit)
 	nums := []int{2, 7, 11, 15}
@@ -93,4 +24,6 @@ func main() {
 			fmt.Printf("nums[%d] doesn't exist!", i)
 		}
 	}
+
+	m.String()
 }
