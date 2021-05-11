124049f0f46bf9d3cea24c05e524289ce9dcb834
Chang Chen Chien <e850506@gmail.com>
2021-05-11 08:15:27 +0800

    Fan-out, fan-in
    Ref:
            - [Go Concurrency Patterns: Pipelines and cancellation](https://blog.golang.org/pipelines#TOC_4.)

--- go/goblog/pipelines/main.go
+++ go/goblog/pipelines/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func gen(nums ...int) <-chan int {
 	out := make(chan int)
@@ -24,12 +27,41 @@ func sq(in <-chan int) <-chan int {
 	return out
 }
 
-func main() {
-	// Set up the pipeline.
-	c := gen(2, 3)
-	out := sq(c)
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
 
-	// Consume the output.
-	fmt.Println(<-out) // 4
-	fmt.Println(<-out) // 9
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	in := gen(2, 3)
+
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// Consume the merged output from c1 and c2.
+	for n := range merge(c1, c2) {
+		fmt.Println(n) // 4 then 9, or 9 then 4
+	}
 }
