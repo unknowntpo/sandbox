09d77a3585de4e2da380a782cb1c13d56f97b856
Chang Chen Chien <e850506@gmail.com>
2021-05-17 07:57:41 +0800

    Explicit cancellation without problem
    Ref:
        - [Go Concurrency Patterns: Pipelines and cancellation](https://blog.golang.org/pipelines#TOC_6.)

--- go/goblog/pipelines/main.go
+++ go/goblog/pipelines/main.go
@@ -5,23 +5,32 @@ import (
 	"sync"
 )
 
-// Rewrite gen to copy the list of integers into a buffered channel and avoid creating a new goroutine.
-func gen(nums ...int) <-chan int {
+func gen(done <-chan struct{}, nums ...int) <-chan int {
 	out := make(chan int, len(nums))
-	for _, n := range nums {
-		out <- n
-	}
-	close(out)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+	}()
 	return out
 }
 
-func sq(in <-chan int) <-chan int {
+func sq(done <-chan struct{}, in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
+		defer close(out)
 		for n := range in {
-			out <- n * n
+			select {
+			case out <- n * n:
+			case <-done:
+				return
+			}
 		}
-		close(out)
 	}()
 	return out
 }
@@ -34,13 +43,14 @@ func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 	// copies values from c to out until c is closed or it receives a value
 	// from done, then output calls wg.Done.
 	output := func(c <-chan int) {
+		defer wg.Done()
 		for n := range c {
 			select {
 			case out <- n:
 			case <-done:
+				return
 			}
 		}
-		wg.Done()
 	}
 
 	wg.Add(len(cs))
@@ -59,18 +69,21 @@ func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 }
 
 func main() {
-	in := gen(2, 3)
+	// Set up a done channel that's shared by the whole pipeline,
+	// and close that channel when this pipeline exits, as a signal
+	// for all the goroutines we started to exit.
+	done := make(chan struct{})
+	defer close(done)
+
+	in := gen(done, 2, 3)
 
 	// Distribute the sq work across two goroutines that both read from in.
-	c1 := sq(in)
-	c2 := sq(in)
+	c1 := sq(done, in)
+	c2 := sq(done, in)
 
 	// Consume the first value from output.
-	done := make(chan struct{}, 2)
 	out := merge(done, c1, c2)
 	fmt.Println(<-out) // 4 or 9
 
-	// Tell the remaining senders we're leaving.
-	done <- struct{}{}
-	done <- struct{}{}
+	// done will be closed by the deferred call.
 }
