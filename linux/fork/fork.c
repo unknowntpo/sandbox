7f4c5d725e9a6d587de400a0dd1d4f7473a76d75
Chang Chen Chien <e850506@gmail.com>
2021-06-17 08:33:13 +0800

    Practice fork(2)

--- linux/fork/fork.c
+++ linux/fork/fork.c
@@ -1,13 +1,32 @@
+/*
+ * My practice of using fork(2) system call
+ */
+#include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-int main()
-{
-    if (fork() == 0) {
-        printf("childish\n");
-        execlp("ls", "ls", "-al", NULL);
-        printf("after execlp ...\n");  // won't be executed
-    } else {
-        printf("I'm parental: my pid = %d/n", getpid());
-    }
+
+pid_t Fork(void); 
+
+int main() {
+  pid_t pid_child;
+  if ((pid_child = Fork()) == 0) {
+    printf("I am child !, my pid is %d, my parent's pid is %d\n", getpid(),
+           getppid());
+    sleep(30);
+  } else {
+    printf("I am parent !, my pid is %d, my child's pid is %d\n", getpid(),
+           pid_child);
+  }
+  sleep(30);
+}
+
+pid_t Fork(void) {
+  pid_t pid;
+
+  if ((pid = fork()) < 0) {
+    perror("fork error");
+  }
+
+  return pid;
 }
