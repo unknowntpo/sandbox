32d71ad3e0ecb4a283799c828ff1bed3b727a33f
Chang Chen Chien <e850506@gmail.com>
2021-06-18 08:05:10 +0800

    Javascript info: Hello, object
    Ref:
        - [Objects](https://javascript.info/object#tasks)

---
+++ javascript/javascript-info/hello-object/app.js
@@ -0,0 +1,18 @@
+// Object literal syntax
+let user = {};
+console.log(user);
+
+// Add the property name with the value John.
+user.name = "Pete";
+console.log(user);
+
+// Add the property surname with the value Smith.
+user.surname = "Smith";
+console.log(user);
+
+// Change the value of the `name` to `Pete`.
+user.name = "Pete";
+console.log(user);
+// Remove the property `name` from the object.
+delete(user.name);
+console.log(user);
