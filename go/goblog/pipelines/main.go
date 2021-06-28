2848c266945998cbe266117d7d0f39c3af2fb773
Chang Chen Chien <e850506@gmail.com>
2021-06-28 10:19:07 +0800

    Use context to wait for job to be done

--- go/goblog/pipelines/main.go
+++ go/goblog/pipelines/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
 )
 
-func gen(done <-chan struct{}, nums ...int) <-chan int {
-	out := make(chan int, len(nums))
+func gen(ctx context.Context, nums ...int) <-chan int {
+	out := make(chan int, 3)
 	go func() {
 		defer close(out)
 		for _, n := range nums {
 			select {
 			case out <- n:
-			case <-done:
+			case <-ctx.Done():
+				fmt.Println("gen is canceled:", ctx.Err())
 				return
 			}
 		}
@@ -20,14 +23,15 @@ func gen(done <-chan struct{}, nums ...int) <-chan int {
 	return out
 }
 
-func sq(done <-chan struct{}, in <-chan int) <-chan int {
+func sq(ctx context.Context, in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
 		defer close(out)
 		for n := range in {
 			select {
 			case out <- n * n:
-			case <-done:
+			case <-ctx.Done():
+				fmt.Println("sq is canceled:", ctx.Err())
 				return
 			}
 		}
@@ -35,7 +39,7 @@ func sq(done <-chan struct{}, in <-chan int) <-chan int {
 	return out
 }
 
-func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
+func merge(ctx context.Context, cs ...<-chan int) <-chan int {
 	var wg sync.WaitGroup
 	out := make(chan int)
 
@@ -47,7 +51,8 @@ func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 		for n := range c {
 			select {
 			case out <- n:
-			case <-done:
+			case <-ctx.Done():
+				fmt.Println("output listener in merge is canceled: ", ctx.Err())
 				return
 			}
 		}
@@ -63,27 +68,47 @@ func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 	// done.  This must start after the wg.Add call.
 	go func() {
 		wg.Wait()
+		fmt.Println("merge is canceled: ", ctx.Err())
 		close(out)
 	}()
 	return out
 }
 
 func main() {
-	// Set up a done channel that's shared by the whole pipeline,
-	// and close that channel when this pipeline exits, as a signal
-	// for all the goroutines we started to exit.
-	done := make(chan struct{})
-	defer close(done)
+	// generate slice of int
+	genNums := func() []int {
+		nums := []int{0}
+		for i := 0; i < 10; i++ {
+			nums = append(nums, i)
+		}
 
-	in := gen(done, 2, 3)
+		return nums
+	}
+
+	nums := genNums()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	in := gen(ctx, nums...)
 
 	// Distribute the sq work across two goroutines that both read from in.
-	c1 := sq(done, in)
-	c2 := sq(done, in)
+	c1 := sq(ctx, in)
+	c2 := sq(ctx, in)
 
 	// Consume the first value from output.
-	out := merge(done, c1, c2)
-	fmt.Println(<-out) // 4 or 9
-
-	// done will be closed by the deferred call.
+	out := merge(ctx, c1, c2)
+	for i := 0; i < len(nums); i++ {
+		select {
+		case num := <-out:
+			fmt.Println(num)
+		case <-ctx.Done():
+			fmt.Println("output printer is canceled: ", ctx.Err())
+			goto end
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+end:
+	time.Sleep(2 * time.Second)
 }
