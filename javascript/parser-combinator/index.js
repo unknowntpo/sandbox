f8e60f5a8cbd238439a3735728606b34fdd6c130
Chang Chen Chien <e850506@gmail.com>
2021-07-05 19:47:58 +0800

    Implementing the mechanics [Parser Combinators From Scratch] Episode 2
    Ref:
        - [Implementing the mechanics [Parser Combinators From Scratch]
          Episode 2](https://www.youtube.com/watch?v=1axJDmK_pnE)

--- javascript/parser-combinator/index.js
+++ javascript/parser-combinator/index.js
@@ -1,11 +1,85 @@
-const A = require('arcsecond')
+const updateParserState = (state, index, result) => ({
+    ...state,
+    index,
+    result
+});
 
-const stringParser = A.many(A.str('hello')).map(
-    results => results.map(
-        x => x.toUpperCase()
-    ).join('')
-)
+const updateParserResult = (state,  result) => ({
+    ...state,
+    result
+});
+
+
+const updateParserError = (state, errMsg) => ({
+    ...state,
+    isError: true,
+    error: errMsg
+});
+
+const str = s => parserState => {
+    const {
+        targetString,
+        index,
+        isError
+    } = parserState;
+
+    if (isError) {
+        return parserState;
+    }
+
+    const slicedTarget = targetString.slice(index)
+
+    if (slicedTarget.length === 0) {
+        return updateParserError(parserState, `str: Tried to match "${s}", but got unexpected end of input`);
+    }
+
+    if (targetString.slice(index)
+        .startsWith(s)) {
+        // Success
+
+        return updateParserState(parserState, index + s.length, s);
+    }
+
+    return updateParserError(
+        parserState,
+        `Tried to match ${s}, but got "${targetString.slice(index, index +10)}"`
+    );
+}
+
+const sequenceOf = parsers => parserState => {
+    if (parserState.isError) {
+        return parserState;
+    }
+
+    const results = [];
+    let nextState = parserState;
+
+    for (let p of parsers) {
+        nextState = p(nextState);
+        results.push(nextState.result);
+    }
+
+    return updateParserResult(nextState, results);
+}
+
+const run = (parser, targetString) => {
+    const initialState = {
+        targetString,
+        index: 0,
+        result: null,
+        isError: false,
+        error: null
+    };
+    return parser(initialState);
+}
+
+
+const parser = sequenceOf([
+    str('hello there!'),
+    str('goodbye there!')
+]);
+
 
 console.log(
-    stringParser.run('hellohellohellohellohellohello')
+    run(parser, 'hello there!goodbye there!')
 )
