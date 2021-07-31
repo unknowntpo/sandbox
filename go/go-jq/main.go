b1519851a6ec370e160d8a1d0a5c2346bfdc0a3e
Chang Chen Chien <e850506@gmail.com>
2021-07-31 08:59:54 +0800

    Bugfix: Correct wrong formatting of json array

--- go/go-jq/main.go
+++ go/go-jq/main.go
@@ -2,10 +2,6 @@ package main
 
 import "fmt"
 
-const in = `{"task": {"id": 1,"title": "hello","content": "do some homework","done": true,"version": 1}}`
-
-const defaultBufSize = 100
-
 type Formatter struct {
 	indent int
 	out    []byte // the buffer where we store the modified output.
@@ -29,8 +25,8 @@ func (f *Formatter) Format(in []byte) {
 			depth--
 			f.out = append(f.out, []byte(fmt.Sprintf("\n%*s}", depth*f.indent, ""))...)
 		case '[':
-			f.out = append(f.out, []byte(fmt.Sprintf("\n%*s[", depth*f.indent, ""))...)
 			depth++
+			f.out = append(f.out, []byte(fmt.Sprintf("[\n%*s", depth*f.indent, ""))...)
 		case ']':
 			depth--
 			f.out = append(f.out, []byte(fmt.Sprintf("\n%*s]", depth*f.indent, ""))...)
@@ -40,8 +36,23 @@ func (f *Formatter) Format(in []byte) {
 
 	}
 }
+
+// Clear clears the output buffer.
+func (f *Formatter) Clear() {
+	f.out = []byte{}
+}
+
 func main() {
+	inputs := []string{
+		`{"task": {"id": 1,"title": "hello","content": "do some homework","done": true,"version": 1}}`,
+		`{"messages": ["hello","hello","hello"]}`,
+	}
+
 	f := NewFormatter(4)
-	f.Format([]byte(in))
-	fmt.Println(string(f.out))
+	for _, in := range inputs {
+		f.Format([]byte(in))
+		fmt.Println(string(f.out))
+		f.Clear()
+		fmt.Println()
+	}
 }
