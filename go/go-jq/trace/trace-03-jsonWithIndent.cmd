875c7dfece67a21084369997d5b8ab4f50fc5423
Chang Chen Chien <e850506@gmail.com>
2021-08-01 03:32:45 +0800

    Add a trace file: json with indent
    Now, gojq doesn't handle the case when input already has indent,
    
    E.g.
    
    $ cat trace/trace-03-jsonWithIndent.cmd| ./gojq
    
    Output:
    {

---
+++ go/go-jq/trace/trace-03-jsonWithIndent.cmd
@@ -0,0 +1,16 @@
+{
+    "ID": "eo7jnzguqgtpdah3cm5srfb97",
+    "Version": {
+      "Index": 17
+    },
+    "CreatedAt": "2017-03-24T08:15:09.735271783Z",
+    "UpdatedAt": "2017-03-24T08:15:09.735271783Z",
+    "Spec": {
+      "Name": "my_config",
+      "Labels": {
+        "env": "dev",
+        "rev": "20170324"
+      },
+      "Data": "aGVsbG8K"
+    }
+}
