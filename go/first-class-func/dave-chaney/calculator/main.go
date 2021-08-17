0d7a5e2f1e9cc24a256b57d4a19c4463d3fe78ef
Chang Chen Chien <e850506@gmail.com>
2021-08-17 19:58:56 +0800

    Change opfunc to func(float) float
    Now in main we call Do not with the Add function itself, but with the result of evaluating Add(10).

--- go/first-class-func/dave-chaney/calculator/main.go
+++ go/first-class-func/dave-chaney/calculator/main.go
@@ -9,26 +9,48 @@ type Calculator struct {
 	acc float64
 }
 
-type opfunc func(float64, float64) float64
+type opfunc func(float64) float64
 
-func (c *Calculator) Do(op opfunc, v float64) float64 {
-	c.acc = op(c.acc, v)
+func (c *Calculator) Do(op opfunc) float64 {
+	c.acc = op(c.acc)
 	return c.acc
 }
 
-func Add(a, b float64) float64 { return a + b }
-func Sub(a, b float64) float64 { return a - b }
-func Mul(a, b float64) float64 { return a * b }
-func Div(a, b float64) float64 { return a / b }
+func Add(n float64) opfunc {
+	return func(acc float64) float64 {
+		return acc + n
+	}
+}
+func Sub(n float64) opfunc {
+	return func(acc float64) float64 {
+		return acc - n
+	}
+}
 
-// TODO: We don't want to pass in useless arguments.
-func Sqrt(n, _ float64) float64 { return math.Sqrt(n) }
+func Mul(n float64) opfunc {
+	return func(acc float64) float64 {
+		return acc * n
+
+	}
+}
+
+func Div(n float64) opfunc {
+	return func(acc float64) float64 {
+		return acc / n
+	}
+}
+
+func Sqrt() opfunc {
+	return func(acc float64) float64 {
+		return math.Sqrt(acc)
+	}
+}
 
 func main() {
 	var c Calculator
-	fmt.Println(c.Do(Add, 100)) // 100
-	fmt.Println(c.Do(Sub, 50))  // 50
-	fmt.Println(c.Do(Mul, 2))   // 100
-	fmt.Println(c.Do(Div, 2))   // 50
-	fmt.Println(c.Do(Sqrt, 0))  // 7.0710678118654755
+	fmt.Println(c.Do(Add(100))) // 100
+	fmt.Println(c.Do(Sub(50)))  // 50
+	fmt.Println(c.Do(Mul(2)))   // 100
+	fmt.Println(c.Do(Div(2)))   // 50
+	fmt.Println(c.Do(Sqrt()))   // 7.0710678118654755
 }
