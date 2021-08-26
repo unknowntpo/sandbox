427b00fdd33a6e77d18219f42b17e8e531c3037b
Chang Chen Chien <e850506@gmail.com>
2021-08-26 14:39:10 +0800

    Test doubles: Fake

--- go/test-doubles/phonebook_test.go
+++ go/test-doubles/phonebook_test.go
@@ -128,3 +128,26 @@ func TestFindCallsSearchAndReturnsPersonUsingMock(t *testing.T) {
 
 	mock.Verify(t)
 }
+
+// Fake: implementations that look like the real thing. But they only do the trick in the scope of the test.
+// e.g. in-memory db.
+type FakeSearcher struct{}
+
+func (fs FakeSearcher) Search(people []*Person, firstName string, lastName string) *Person {
+	if len(people) == 0 {
+		return nil
+	}
+
+	return people[0]
+}
+
+func TestFindCallsSearchAndReturnsEmptyStringForNoPerson(t *testing.T) {
+	phonebook := &Phonebook{}
+	fake := &FakeSearcher{}
+
+	phone, _ := phonebook.Find(fake, "Jane", "Doe")
+
+	if phone != "" {
+		t.Errorf("Wanted '', got '%s'", phone)
+	}
+}
