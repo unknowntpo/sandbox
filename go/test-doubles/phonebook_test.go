67418a2acab2a439064c243bb352b9ea50aeb5ed
Chang Chen Chien <e850506@gmail.com>
2021-08-26 14:32:48 +0800

    test double: Add stub

--- go/test-doubles/phonebook_test.go
+++ go/test-doubles/phonebook_test.go
@@ -4,7 +4,8 @@ import (
 	"testing"
 )
 
-// DummySearcher is a dummy instance for searcher interface, all we need is an instance that we can just pass in, and the compiler won’t complain.
+// DummySearcher is a dummy instance for searcher interface,
+// all we need is an instance that we can just pass in, and the compiler won’t complain.
 type DummySearcher struct{}
 
 func (ds DummySearcher) Search(people []*Person, firstName, lastName string) *Person {
@@ -19,5 +20,32 @@ func TestFindReturnsError(t *testing.T) {
 
 	if got != want {
 		t.Errorf("Want '%s', got '%s'", want, got)
+	}
+}
+
+// When we want to make the Searcher implementation return an actual value that we can assert against,
+// we need a stub. Instead of understanding what it would take to set up and/or implement a proper Searcher,
+// you just create a stub implementation that returns only one value.
+// This is the idea behind stubs.
+type StubSearcher struct {
+	phone string
+}
+
+func (ss StubSearcher) Search(people []*Person, firstName, lastName string) *Person {
+	return &Person{
+		FirstName: firstName,
+		LastName:  lastName,
+		Phone:     ss.phone,
+	}
+}
+
+func TestFindReturnsPerson(t *testing.T) {
+	fakePhone := "+31 65 222 333"
+	phonebook := &Phonebook{}
+
+	phone, _ := phonebook.Find(StubSearcher{fakePhone}, "Jane", "Doe")
+
+	if phone != fakePhone {
+		t.Errorf("Want '%s', got '%s'", fakePhone, phone)
 	}
 }
