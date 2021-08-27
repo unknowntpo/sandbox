752c5e89a2febb079a5810c49fdb3734958daa5f
Chang Chen Chien <e850506@gmail.com>
2021-08-27 13:19:28 +0800

    Add basic structure of makefile in csapp playground

--- csapp/include/csapp.h
+++ csapp/include/csapp.h
@@ -1,6 +1,17 @@
 #ifndef __CSAPP_H__
 #define __CSAPP_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <sys/stat.h>
+
+/* Our own error-handling functions */
+void unix_error(char *msg);
+
+/* Unix I/O wrappers */
+void Stat(const char *filename, struct stat *buf);
 void Fstat(int fd, struct stat *buf);
 
 #endif /* __CSAPP_H__ */
--- csapp/src/csapp.c
+++ csapp/src/csapp.c
@@ -1,12 +1,20 @@
 #include "csapp.h"
 
-// From learn c the hard way ex19
-#define log_err(M, ...)                                                       \
-    fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, \
-            clean_errno(), ##__VA_ARGS__)
+/* Unix style error */
+void unix_error(char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+
+void Stat(const char *filename, struct stat *buf)
+{
+    if (stat(filename, buf) < 0)
+        unix_error("Stat error");
+}
 
 void Fstat(int fd, struct stat *buf)
 {
     if (fstat(fd, buf) < 0)
-        log_err("Fstat error");
+        unix_error("Fstat error");
 }
