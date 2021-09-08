fd41f10771cbd8e7cacd4f50f82973a321868df6
Chang Chen Chien <e850506@gmail.com>
2021-09-08 09:07:36 +0800

    Use zerolog with pkg/errors
    We got result:
    
    {
      "level": "error",
      "stack": [
        {
          "func": "inner",
          "line": "20",
          "source": "main.go"
        },
        {
          "func": "middle",
          "line": "24",
          "source": "main.go"
        },
        {
          "func": "outer",
          "line": "32",
          "source": "main.go"
        },
        {
          "func": "main",
          "line": "15",
          "source": "main.go"
        },
        {
          "func": "main",
          "line": "225",
          "source": "proc.go"
        },
        {
          "func": "goexit",
          "line": "1371",
          "source": "asm_amd64.s"
        }
      ],
      "error": "seems we have an error here",
      "time": 1631063034
    }

---
+++ go/zerolog/stacktrace/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/pkgerrors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	err := outer()
+	log.Error().Stack().Err(err).Msg("")
+}
+
+func inner() error {
+	return errors.New("seems we have an error here")
+}
+
+func middle() error {
+	err := inner()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func outer() error {
+	err := middle()
+	if err != nil {
+		return err
+	}
+	return nil
+}
