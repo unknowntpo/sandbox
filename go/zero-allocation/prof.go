c6263d9c0d6e1957d4d8312d95adfb1111e4ac0e
Chang Chen Chien <e850506@gmail.com>
2021-09-09 01:45:43 +0800

    Use sync.Pool to make buf act like global obj
    sync.Pool is a feature in Go that allows you to amortize allocation cost.
    With our current code, when we allocate the buffers,
    we do so for the lifetime of the foo function.
    But what if we were able to allocate it once, store it in a global variable,
    then just reuse it infinitely (resetting the contents every time while keeping the capacity)?
    Doing this manually would be a little complicated and also wouldn’t work concurrently.
    That’s what sync.Pool is for. sync.Pool allows you to Get an already allocated object
    and Put it back when you are done with it.
    If there are no objects availabile in the pool and you request one,
    it will call the New function you define to allocate one.
    Let’s create a pool for the hash object and one for the hash sum (256 byte slice).
    
    Ref:
        - [https://chris124567.github.io/2021-06-21-go-performance/]

--- go/zero-allocation/prof.go
+++ go/zero-allocation/prof.go
@@ -3,30 +3,61 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"hash"
 	"math/rand"
 	"os"
 	"runtime/pprof"
 	"strconv"
+	"sync"
 	"testing"
 	"unsafe"
 )
 
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		// length of a sha256 hash
+		b := make([]byte, 256)
+		return &b
+	},
+}
+
+var hashPool = sync.Pool{
+	New: func() interface{} {
+		return sha256.New()
+	},
+}
+
 func foo(n int) string {
+	// get buffer from pool
+	bufptr := bufPool.Get().(*[]byte)
+	defer bufPool.Put(bufptr)
+	buf := *bufptr
+	// reset buf
+	buf = buf[:0]
 
-	x := strconv.Itoa(n)
-	buf := make([]byte, 0, 100000*len(x))
+	// get hash object from pool
+	h := hashPool.Get().(hash.Hash)
+	defer hashPool.Put(h)
+	h.Reset()
+
+	x := strconv.AppendInt(buf, int64(n), 10)
+	buf = make([]byte, 0, 100000*len(x))
 	for i := 0; i < 100000; i++ {
-		buf = append(buf, x...)
+		h.Write(x)
 	}
-	sum := sha256.Sum256(buf)
+	// reset whatever strconv.AppendInt put in the buf
+	buf = buf[:0]
+	sum := h.Sum(buf)
 
-	b := make([]byte, 0, int(sum[0]))
+	b := make([]byte, 0, 256)
 	for i := 0; i < int(sum[0]); i++ {
 		x := sum[(i*7+1)%len(sum)] ^ sum[(i*5+3)%len(sum)]
 		c := "abcdefghijklmnopqrstuvwxyz"[x%26]
 		b = append(b, c)
 	}
-	return *(*string)(unsafe.Pointer(&b))
+	sum = sum[:0] // reset sum
+	sum = append(sum, b...)
+	return *(*string)(unsafe.Pointer(&sum))
 }
 
 func main() {
