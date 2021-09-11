89a50ebca67a340869f37c23e1a2cfb62bd04a09
Chang Chen Chien <e850506@gmail.com>
2021-09-11 13:46:44 +0800

    Add experiment of zerolog stack trace features

--- go/zerolog/stacktrace/main.go
+++ go/zerolog/stacktrace/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/pkgerrors"
 
@@ -8,22 +10,32 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var ErrDummy = errors.New("dummy error")
+
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 
 	err := outer()
 	log.Error().Stack().Err(err).Msg("")
+
+	fmt.Println("output from fmt.Printf")
+	fmt.Printf("%+v", err)
 }
 
 func inner() error {
-	return errors.New("seems we have an error here")
+	// Record the stack.
+	//return errors.Wrap(ErrDummy, "inner error")
+	//return errors.WithMessage(ErrDummy, "inner error")
+	return ErrDummy
 }
 
 func middle() error {
 	err := inner()
 	if err != nil {
-		return err
+		return errors.Errorf("middle error: %v", err)
+		//return errors.WithMessage(err, "middle error")
+		//return errors.Wrap(err, "middle")
 	}
 	return nil
 }
@@ -31,7 +43,11 @@ func middle() error {
 func outer() error {
 	err := middle()
 	if err != nil {
-		return err
+		return errors.Errorf("outer error: %v", err)
+		//return errors.WithMessage(err, "outer error")
+		//return errors.Wrap(err, "outer")
+		//return errors.WithMessage(err, "outer error")
+
 	}
 	return nil
 }
