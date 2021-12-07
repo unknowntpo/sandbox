c7134f4b93b5a5bf9a47372a01e28cf9f59e7ca8
Chang Chen Chien <e850506@gmail.com>
2021-12-07 10:02:57 +0800

    Add example: DB Migration In Go Lang
    Ref:
        - [DB Migration In Go Lang](https://medium.com/geekculture/db-migration-in-go-lang-d325effc55de)

---
+++ go/migrate-playground/README.md
@@ -0,0 +1,2 @@
+DB Migration In Go Lang
+link https://medium.com/geekculture/db-migration-in-go-lang-d325effc55de
---
+++ go/migrate-playground/db/migration/000001_playground_schema.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+   id BIGSERIAL primary key,
+   first_name TEXT not null,
+   last_name TEXT,
+   created_at TIMESTAMP default now()
+);
