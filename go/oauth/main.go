03eb90d6960a598d8cf968539ed305a3bf2f7c8c
Chang Chen Chien <e850506@gmail.com>
2021-12-23 08:53:18 +0800

    Finish callback handler
    TODO: parse response and access user's data with access_token

--- go/oauth/main.go
+++ go/oauth/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -16,7 +17,6 @@ var (
 )
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello"))
 	/*
 		GET https://github.com/login/oauth/authorize
 		client_id= ...
@@ -28,16 +28,21 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	_ = redirURL
 	q := url.Values{}
 	q.Add("client_id", fmt.Sprint(CLIENT_ID))
-	q.Add("redirect_uri", fmt.Sprintf("http://localhost:%d", port))
+	q.Add("redirect_uri", fmt.Sprintf("http://localhost:%d/callback", port))
 	q.Add("scope", "user, repo")
 	q.Add("allow_signup", "true")
 	rawQuery := q.Encode()
-	fmt.Println(rawQuery)
+	//fmt.Println(rawQuery)
 	// TODO: redirect user to that url
+	redirURL += "?" + rawQuery
+	fmt.Println(redirURL)
+	http.Redirect(w, r, redirURL, http.StatusSeeOther)
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello"))
+	//w.Write([]byte("hello"))
+
+	// extract code from r params
 	// send request to get access token
 	// POST https://github.com/login/oauth/access_token
 	// params:
@@ -45,7 +50,42 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	// client_secret
 	// code
 	// redirect
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		fmt.Fprintf(w, "failed to get code from url parameter")
+		return
+	}
+	fmt.Fprintf(w, "code: %s", code)
 
+	client := http.Client{}
+
+	r, err := http.NewRequest(http.MethodPost, "https://github.com/login/oauth/access_token", nil)
+	if err != nil {
+		fmt.Fprintf(w, "failed to send request: %v", err)
+		return
+	}
+	q := url.Values{}
+	q.Add("code", code)
+	q.Add("client_id", CLIENT_ID)
+	q.Add("client_secret", CLIENT_SECRET)
+	q.Add("redirect_uri", "http://localhost:4444/callback")
+
+	r.URL.RawQuery = q.Encode()
+
+	r.Header.Set("Accept", "application/json")
+
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Fprintf(w, "failed to send request: %v", err)
+		return
+	}
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(w, "failed to read resp body: %v", err)
+		return
+	}
+	fmt.Println("response: \n", string(b))
+	// TODO: parse response body and access github user repo
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
