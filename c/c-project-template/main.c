5405edd0692fe4057bd094d010d1fb7a326c6ba2
Chang Chen Chien <e850506@gmail.com>
2022-01-19 10:50:32 +0800

    Add minunit
    Ref:
        - [JTN002 - MinUnit -- a minimal unit testing framework for C](https://jera.com/techinfo/jtns/jtn002)

--- c/c-project-template/main.c
+++ c/c-project-template/main.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 
 #include "counter.h"
+#include "minunit.h"
+
+int tests_run = 0;
+
+static char *test_counter()
+{
+    cnt_t cnt = 0;
+    add(&cnt);
+    int got = cnt;
+    int want = 1;
+    mu_assert("wrong counter value: got %d, want %d", got == want);
+    return 0;
+}
+
+static char *test_add()
+{
+    mu_assert("3 + 2 should be 5", 3 + 2 == 6);
+    return 0;
+}
+
+static char *all_tests()
+{
+    mu_run_test(test_counter);
+    mu_run_test(test_add);
+    return 0;
+}
 
 int main(int argc, const char *argv[])
 {
-    cnt_t cnt;
-    add(&cnt);
-    printf("counter: %d\n", cnt);
+    char *result = all_tests();
+    if (result != 0) {
+        printf("FAIL\n");
+        printf("%s\n", result);
+    } else {
+        printf("PASS\n");
+    }
+    printf("Tests run: %d\n", tests_run);
 
-    return 0;
+    return result != 0;
 }
