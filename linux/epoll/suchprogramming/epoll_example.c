ca5689f7b092610d1a4db38a6dc319caaabf350d
Chang Chen Chien <e850506@gmail.com>
2022-01-22 14:32:25 +0800

    Step 3: Profit
    Ref: [epoll() Tutorial - epol() in 3 easy Steps](https://suchprogramming.com/epoll-in-3-easy-steps/)

--- linux/epoll/suchprogramming/epoll_example.c
+++ linux/epoll/suchprogramming/epoll_example.c
@@ -3,9 +3,15 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 
+#define MAX_EVENTS 5
+#define READ_SIZE 10
+
 int main()
 {
-    struct epoll_event event;
+    int running = 1, event_count, i;
+    size_t bytes_read;
+    char read_buffer[READ_SIZE + 1];
+    struct epoll_event event, events[MAX_EVENTS];
     int epoll_fd = epoll_create1(0);
 
     if (epoll_fd == -1) {
@@ -20,6 +26,22 @@ int main()
         fprintf(stderr, "failed to add file descripter to epoll\n");
         close(epoll_fd);
         return 1;
+    }
+
+    while (running) {
+        printf("\nPolling for input ...\n");
+        event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
+        printf("%d ready events\n", event_count);
+        for (i = 0; i < event_count; i++) {
+            printf("Reading file descriptor '%d' -- ", events[i].data.fd);
+            bytes_read = read(events[i].data.fd, read_buffer, READ_SIZE);
+            printf("%zd bytes read.\b", bytes_read);
+            read_buffer[bytes_read] = '\0';
+            printf("Read '%s'\n", read_buffer);
+
+            if (!strncmp(read_buffer, "stop\n", 5))
+                running = 0;
+        }
     }
 
     if (close(epoll_fd)) {
