2cc072774c3ef4f9c552c9b95613ee5c2576c1b4
Chang Chen Chien <e850506@gmail.com>
2022-01-23 13:45:01 +0800

    Add lua script TODO: How to log to stdout ?

--- redis/redis-essentials/lua/ch1_articles-popularity.lua
+++ redis/redis-essentials/lua/ch1_articles-popularity.lua
@@ -1,8 +1,7 @@
-
-
 local article = "article"
 local votes = "votes"
-local head = "head-line"
+-- local head = "head-line"
+--
 
 -- function upVote(id)
 
@@ -11,11 +10,26 @@ local function upVote(id)
     redis.call("INCR", key)
 end
 -- function downVote(id)
+
+local function downVote(id)
+    local key = article .. ":" .. id .. ":" .. votes
+    redis.call("DECR", key)
+end
+
 -- function showResults(id)
 local function showResults(id)
     local voteKey = article .. ":" .. id .. ":"  .. votes 
-    return redis.call("GET", voteKey)
+    -- How log to stdout ?
+    redis.log(redis.LOG_WARNING, "The article" .. id .. "has" .. redis.call("GET", voteKey) .. "votes")
 end
---
-upVote(3)
-print(showResults(3))
+
+upVote(12345) -- article:12345 has 1 vote
+upVote(12345) -- article:12345 has 2 votes
+upVote(12345) -- article:12345 has 3 votes
+upVote(10001) -- article:10001 has 1 vote
+upVote(10001) -- article:10001 has 2 votes
+downVote(10001) -- article:10001 has 1 vote
+upVote(60056) -- article:60056 has 1 vote
+showResults(12345)
+showResults(10001)
+showResults(60056)
