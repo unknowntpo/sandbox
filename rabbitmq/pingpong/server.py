ef017b07966abe30b59ddcc8c1b796f5d518bddf
Chang Chen Chien <e850506@gmail.com>
2022-01-24 10:08:29 +0800

    Fix bug

--- rabbitmq/pingpong/server.py
+++ rabbitmq/pingpong/server.py
@@ -4,8 +4,9 @@ import pika
 def callback(ch, method, property, body):
     print("server got ping")
     ch.basic_ack(delivery_tag = method.delivery_tag)
+    channel.basic_publish(exchange='', routing_key='pong', body=msg)
 
-msg = "ping"
+msg = "pong"
 
 # Set up connection
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -18,6 +19,5 @@ channel.queue_declare(queue='pong', durable=True)
 # send message 
 
 channel.basic_consume(queue='ping', on_message_callback=callback)
-channel.basic_publish(exchange='', routing_key='pong', body=msg)
 
 channel.start_consuming()
